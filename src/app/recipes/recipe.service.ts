995447ef2e758677e5650dec0c68dc3a16d55da7
Crystal <crystal120599@gmail.com>
2023-12-21 22:33:39 -0800

    manage arrays of recipes from service to parent to child

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,13 @@
+import { Recipe } from "./recipe.model";
+// provide service in the recipes component as a start
 export class RecipeService{
-    
+    // add seed data from recipe-list
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    ];
+    getRecipes(){
+        // returns new copy of array
+        return this.recipes.slice();
+    }
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
+// this component and children of it will share same instance of service but wont
+// access or same instance in other components
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
+  providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
